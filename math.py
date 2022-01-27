a85b1788c6629118e658530cbdf0c873df7519e2
Sanju9980 <98524952+Sanju9980@users.noreply.github.com>
2022-01-27 16:47:42 +0530

    added math.py

---
+++ math.py
@@ -0,0 +1,14 @@
+#add implementaion
+def add(x.y):
+	return x+y
+#subtract implementaion
+def subtract(x.y):
+	return  x-y #on master branch
+#multiply implementaion
+def multiply(x.y):
+	return x*y # on bug456
+#divide implementaion
+def divide(x.y):
+	if(x/y==0)
+		return error
+
